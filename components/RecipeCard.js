4f99d1e5d64d9710ef0d8438cab95750601222ab
Rodrigo Lins <rlinsdev@gmail.com>
2021-04-15 08:35:15 -0300

    link contentful with application

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,11 +1,15 @@
 import Link from 'next/link'
+import Image from 'next/image'
 export default function RecipeCard({recipe}) {
-  const {title, slug, cookieTime, thunbnail } = recipe.fields
+  const {title, slug, cookieTime, thumbmail } = recipe.fields
 
   return (
     <div className="card">
       <div className="feature">
-        {/* {img} */}
+        <Image 
+          src={'https:'+thumbmail.fields.file.url}
+          width={thumbmail.fields.file.details.image.width}
+          height={thumbmail.fields.file.details.image.height} />
       </div>
       <div className="content">
         <div className="info">
