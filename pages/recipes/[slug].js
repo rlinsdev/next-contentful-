0b793b55becdcb84e29e703b1ec55ef8d66c9ec4
Rodrigo Lins <rlinsdev@gmail.com>
2021-04-26 16:31:06 -0300

    Featuring the style of [slugs]

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -48,7 +48,7 @@ export default function RecipeDetails({recipe}) {
         <h2>{title}</h2>
 
       </div>
-      <div classn="info">
+      <div className="info">
         <p>Take about {cookingTime} mins to cook.</p>
         <h3>Ingredients</h3>
         {ingredients.map(ing =>(
@@ -59,6 +59,31 @@ export default function RecipeDetails({recipe}) {
         <h3>Method: </h3>
         <div>{documentToReactComponents(method)}</div>
       </div>
+      <style jsx>{`
+        h2,h3 {
+          text-transform: uppercase;
+        }
+        .banner h2 {
+          margin: 0;
+          background: #fff;
+          display: inline-block;
+          padding: 20px;
+          position: relative;
+          top: -60px;
+          left: -10px;
+          transform: rotateZ(-1deg);
+          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+        }
+        .info p {
+          margin: 0;
+        }
+        .info span::after {
+          content: ", ";
+        }
+        .info span:last-child::after {
+          content: ".";
+        }
+      `}</style>
     </div>
   )
 }
